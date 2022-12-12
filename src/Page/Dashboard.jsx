bb3e264466088894698227e0d3f12d410c526c75
thanhphuong2510 <98444115+ThanhPhuonng2510@users.noreply.github.com>
2022-12-13 02:10:51 +0700

    Check

---
+++ src/Page/Dashboard.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "../Conpoments/SideBar/Sidebar";
+
+const Dashboard = () => {
+    return (
+        <section className="flex gap-6">
+        <Sidebar/>
+        </section>
+    )
+}
+export default Dashboard;
